4d5f46db320ea50304309b7e54a5ff164db691a0
La-Vanille <1489377390@qq.com>
2023-04-17 19:05:34 +0800

    homepage添加日期时钟

---
+++ src/main/java/Controller/DateTimePanel.java
@@ -0,0 +1,43 @@
+package main.java.Controller;
+
+import javax.swing.*;
+import java.awt.*;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+public class DateTimePanel extends JPanel {
+    private JLabel dateTimeLabel;
+
+    public DateTimePanel() {
+        // set the layout of the panel
+        setLayout(new BorderLayout());
+
+        // create a label to display the time and date
+        dateTimeLabel = new JLabel();
+        dateTimeLabel.setHorizontalAlignment(JLabel.CENTER);
+        dateTimeLabel.setFont(new Font("Segoe Script", Font.BOLD, 18));
+        dateTimeLabel.setForeground(Color.WHITE);
+
+        // add the label to the panel
+        add(dateTimeLabel, BorderLayout.CENTER);
+
+        // update the label with the current time and date
+        updateTimeAndDate();
+
+        // start a timer to update the time and date every second
+        Timer timer = new Timer(1000, e -> updateTimeAndDate());
+        timer.start();
+    }
+
+    private void updateTimeAndDate() {
+        // create a date object with the current time
+        Date now = new Date();
+
+        // create a date format to display the time and date
+        SimpleDateFormat dateFormat = new SimpleDateFormat("EEEE, MMMM d, yyyy 'at' h:mm:ss a", Locale.ENGLISH);
+
+        // update the label with the formatted date and time
+        dateTimeLabel.setText(dateFormat.format(now));
+    }
+}
--- src/main/java/Pages/HomePage.java
+++ src/main/java/Pages/HomePage.java
@@ -1,6 +1,7 @@
 package main.java.Pages;
 
-import main.java.Entity.Achievement;
+import com.formdev.flatlaf.FlatDarculaLaf;
+import main.java.Controller.DateTimePanel;
 
 import javax.imageio.ImageIO;
 import javax.swing.*;
@@ -14,75 +15,104 @@ public class HomePage extends JFrame {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setBounds(500,300,1094,729);
         setLocationRelativeTo(null);
-
         // Set the background image
+
         try {
             JLabel backgroundImage = new JLabel(new ImageIcon(ImageIO.read(new File("src/main/resources/images/homepage.JPG"))));
             backgroundImage.setLayout(new GridBagLayout());
+            JButton btnModulePage = new JButton("ModulePage");
+            JButton btnGradePage = new JButton("GradePage");
+            JButton btnAchievement = new JButton("Achievement");
+            JButton btnBack = new JButton("Back");
+
+            // 为按钮添加事件监听器
+            btnModulePage.addActionListener(e -> {
+                new ModulePage().setVisible(true);
+                dispose();
+            });
+
+            btnGradePage.addActionListener(e -> {
+                new GradePage().setVisible(true);
+                dispose();
+            });
+
+            btnAchievement.addActionListener(e -> {
+                new AchievementPage().setVisible(true);
+                dispose();
+            });
+
+//        btnAchievement.addActionListener(new ActionListener() {
+//            @Override
+//            public void actionPerformed(ActionEvent e) {
+//                new Achievement().setVisible(true);
+//                dispose();
+//            }
+//        });
+
+            btnBack.addActionListener(e -> {
+                new LogInPage().setVisible(true);
+                dispose();
+            });
+
+            // 设置按钮颜色和文字颜色
+            Color btnModulePageColor = new Color(96,33,46);
+            Color btnGradePageColor = new Color(179,122,54);
+            Color btnAchievementColor = new Color(25,60,79);
+            Color btnBackColor = new Color(34,68,49);
+            Color buttonTextColor = Color.WHITE;
+            Font buttonFont = new Font("Segoe Script", Font.BOLD, 25);
+
+            btnModulePage.setFont(buttonFont);
+            btnModulePage.setBackground(btnModulePageColor);
+            btnModulePage.setForeground(buttonTextColor);
+
+            btnGradePage.setFont(buttonFont);
+            btnGradePage.setBackground(btnGradePageColor);
+            btnGradePage.setForeground(buttonTextColor);
+
+            btnAchievement.setFont(buttonFont);
+            btnAchievement.setBackground(btnAchievementColor);
+            btnAchievement.setForeground(buttonTextColor);
+
+            btnBack.setFont(buttonFont);
+            btnBack.setBackground(btnBackColor);
+            btnBack.setForeground(buttonTextColor);
+
+            JPanel panel = new JPanel(new FlowLayout());
+
+            DateTimePanel DTpanel = new DateTimePanel();
+            setLayout(new BoxLayout(getContentPane(), BoxLayout.Y_AXIS));
+            // 将按钮添加到窗口
+            panel.add(btnModulePage);
+            panel.add(btnGradePage);
+            panel.add(btnAchievement);
+            panel.add(btnBack);
+            panel.setOpaque(false);
+            DTpanel.setOpaque(false);
+
+            add(DTpanel);
+            add(panel);
+
+            GridBagConstraints c = new GridBagConstraints();
+            c.gridx = 0;
+            c.gridy = 0;
+            c.insets = new Insets(50, 0, 0, 0);
+            backgroundImage.add(DTpanel, c);
+
+            c.gridx = 0;
+            c.gridy = 1;
+            c.insets = new Insets(20, 0, 0, 0);
+            backgroundImage.add(panel, c);
+
             setContentPane(backgroundImage);
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        // 创建三个按钮
-        JButton btnModulePage = new JButton("ModulePage");
-        JButton btnGradePage = new JButton("GradePage");
-        JButton btnAchievement = new JButton("Achievement");
-        JButton btnBack = new JButton("Log out");
-
-        // 为按钮添加事件监听器
-        btnModulePage.addActionListener(e -> {
-            new ModulePage().setVisible(true);
-            dispose();
-        });
-
-        btnGradePage.addActionListener(e -> {
-            new GradePage().setVisible(true);
-            dispose();
-        });
-
-        btnAchievement.addActionListener(e -> {
-            new AchievementPage().setVisible(true);
-            dispose();
-        });
-
-        btnBack.addActionListener(e -> {
-            new LogInPage().setVisible(true);
-            dispose();
-        });
-
-        // 设置按钮颜色和文字颜色
-        Color btnModulePageColor = new Color(96,33,46);
-        Color btnGradePageColor = new Color(179,122,54);
-        Color btnAchievementColor = new Color(25,60,79);
-        Color btnBackColor = new Color(34,68,49);
-        Color buttonTextColor = Color.WHITE;
-        Font buttonFont = new Font("Segoe Script", Font.BOLD, 25);
-
-        btnModulePage.setFont(buttonFont);
-        btnModulePage.setBackground(btnModulePageColor);
-        btnModulePage.setForeground(buttonTextColor);
-
-        btnGradePage.setFont(buttonFont);
-        btnGradePage.setBackground(btnGradePageColor);
-        btnGradePage.setForeground(buttonTextColor);
-
-        btnAchievement.setFont(buttonFont);
-        btnAchievement.setBackground(btnAchievementColor);
-        btnAchievement.setForeground(buttonTextColor);
-
-        btnBack.setFont(buttonFont);
-        btnBack.setBackground(btnBackColor);
-        btnBack.setForeground(buttonTextColor);
-
-        // 将按钮添加到窗口
-        add(btnModulePage);
-        add(btnGradePage);
-        add(btnAchievement);
-        add(btnBack);
     }
 
     public static void main(String[] args) {
+        FlatDarculaLaf.setup();
         SwingUtilities.invokeLater(() -> new HomePage().setVisible(true));
     }
 }
